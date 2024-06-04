367310516b6c93318158fd26b4c4632222a88b08
saravananram558 <saravananram558@gmail.com>
2024-06-04 21:20:16 +0530

    Login and sign screen ui issues fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/all-events',
+    redirectTo: '/register/login',
     pathMatch: 'full'
   },
   {
--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, Event, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-folder',
@@ -7,12 +7,29 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./folder.page.scss'],
 })
 export class FolderPage implements OnInit {
+  public currentUrl!: string;
+  public appPages = [
+    { title: 'All Events', url: '/folder/all-events', icon: 'home' },
+    { title: 'Dashboard', url: '/folder/dashboard', icon: 'stats-chart' },
+    { title: 'Masters', url: '/folder/masters', icon: 'shield' },
+    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
+  ];
   public folder!: string;
   showEventPage: boolean = false;
 
-  constructor(private activatedRoute: ActivatedRoute) {}
+  constructor(private activatedRoute: ActivatedRoute,private router: Router) {}
 
-  ngOnInit() {
+  ngOnInit():void {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        const urlSegments = event.url.split('?');
+        if (urlSegments.length > 0 && urlSegments[0].startsWith('/folder/event')) {
+          this.currentUrl = '/folder/event';
+        } else {
+          this.currentUrl = event.url;
+        }
+      }
+    });
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
     const name = this.activatedRoute.snapshot.queryParamMap.get('name');
     if(this.folder === 'all-events'){
@@ -22,5 +39,9 @@ export class FolderPage implements OnInit {
       this.folder = name;
       this.showEventPage = true;
     }
+  }
+
+  isSelected(url: string): boolean {
+    return this.currentUrl === url;
   }
 }
